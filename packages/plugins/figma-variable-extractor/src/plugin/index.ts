5deb7fc7ccec5b0b0481509a4b61a5a30d8110a1
gomgun-lab <zxolqs@gmail.com>
2024-09-19 23:42:43 +0900

    convert token object to nested object

--- packages/plugins/figma-variable-extractor/src/plugin/index.ts
+++ packages/plugins/figma-variable-extractor/src/plugin/index.ts
@@ -1,6 +1,6 @@
 import { ExtractTokenAction } from "../types/action";
 import { AppMessage } from "../types/plugin";
-import { TokenFile, TokenValue } from "../types/token";
+import { TokenBody, TokenFile, TokenValue } from "../types/token";
 import { responseExtractTokensToUI } from "./lib/messaging";
 import { isRGBA, isVariableAlias, rgbToHex } from "./utils";
 
@@ -18,45 +18,48 @@ async function extractTokens(): Promise<TokenFile[]> {
   const files: TokenFile[] = [];
 
   for (const collection of collections) {
-    files.push(...(await processCollection(collection)));
+    files.push(await processCollection(collection));
   }
 
-  console.log("files: ", files);
+  files.forEach((file) => {
+    file.body = convertToNested(file.body);
+  });
 
   return files;
 }
 
-async function processCollection(collection: VariableCollection): Promise<TokenFile[]> {
-  const files: TokenFile[] = [];
+async function processCollection(
+  collection: VariableCollection
+): Promise<TokenFile> {
+  const file: TokenFile = {
+    fileName: `${collection.name}.tokens.json`,
+    body: {},
+  };
 
   for (const mode of collection.modes) {
-    const file: TokenFile = {
-      fileName:
-        collection.modes.length === 1
-          ? `${collection.name}.tokens.json`
-          : `${collection.name}.${mode.name}.tokens.json`,
-      body: {},
-    };
-
     for (const variableId of collection.variableIds) {
       const variable = await figma.variables.getVariableByIdAsync(variableId);
+
       if (variable) {
         const { name, resolvedType, valuesByMode } = variable;
         const value = valuesByMode[mode.modeId];
+        const token = await createToken(resolvedType, value);
 
-        if (value !== undefined) {
-          const token = await createToken(resolvedType, value);
-          if (token) {
+        if (token) {
+          if (token.$type === "COLOR") {
+            file.body[name.replace(/\//g, ".")] = {
+              ...file.body[name.replace(/\//g, ".")],
+              [mode.name]: token,
+            };
+          } else {
             file.body[name.replace(/\//g, ".")] = token;
           }
         }
       }
     }
-
-    files.push(file);
   }
 
-  return files;
+  return file;
 }
 
 async function createToken(
@@ -65,7 +68,9 @@ async function createToken(
 ): Promise<TokenValue | null> {
   switch (resolvedType) {
     case "BOOLEAN":
+      return { $type: "BOOLEAN", $value: value };
     case "STRING":
+      return { $type: "STRING", $value: value };
     case "COLOR":
       if (isRGBA(value)) {
         return { $type: "COLOR", $value: rgbToHex(value) };
@@ -83,9 +88,37 @@ async function createToken(
   if (isVariableAlias(value)) {
     const variable = await figma.variables.getVariableByIdAsync(value.id);
     if (variable) {
-      return { $type: resolvedType, $value: `{${variable.name.replace(/\//g, ".")}}` };
+      return {
+        $type: resolvedType,
+        $value: `{${variable.name.replace(/\//g, ".")}}`,
+      };
     }
   }
 
   return null;
+}
+
+function convertToNested(obj: TokenBody): TokenBody {
+  const result: TokenBody = {};
+
+  for (const key in obj) {
+    const parts: string[] = key.split(".");
+    let current: TokenBody = result;
+    parts.forEach((part, index) => {
+      if (index === parts.length - 1) {
+        current[part] = obj[key];
+      } else {
+        if (
+          !current[part] ||
+          typeof current[part] !== "object" ||
+          "$type" in current[part]
+        ) {
+          current[part] = {};
+        }
+        current = current[part] as TokenBody;
+      }
+    });
+  }
+
+  return result;
 }
