27ba2233bf6d99d6eab9f6cea9ba5a022852b4ed
gomgun <zxolqs@gmail.com>
2024-09-28 01:59:58 +0900

    fix: change how lint-stage, husky is applied (#6)

--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Type check TypeScript files
-  "(app|packages)/**/*.(ts|tsx)": () => "pnpm run type-check",
-
-  "(app|packages)/**/*.(ts|tsx|js)": filenames => [
-    `eslint --fix ${filenames.join(" ")}`,
-    `pnpx prettier --write ${filenames.join(" ")}`,
-  ],
-};
