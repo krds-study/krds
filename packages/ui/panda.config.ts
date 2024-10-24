a9950a60a0bce1f814071d21704a7732252e37b7
whdgur5717 <whdgur5717@gmail.com>
2024-10-24 22:31:32 +0900

    chore: remove unnecessary properties in panda config

--- packages/ui/panda.config.ts
+++ packages/ui/panda.config.ts
@@ -3,16 +3,11 @@ import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
   preflight: true,
-  presets: [customPreset],
+  presets: ["@pandacss/preset-base", customPreset()],
   conditions: {
     light: "[data-color-mode=light] &",
     dark: "[data-color-mode=dark] &",
   },
-  eject: true,
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
-  exclude: [],
-  strictTokens: true,
-  minify: true,
-  clean: true,
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./src/stories/*.{js,jsx,ts,tsx}"],
   jsxFramework: "react",
 });
