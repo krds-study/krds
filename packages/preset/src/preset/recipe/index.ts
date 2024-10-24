62ddb6b950963891bb9ab8e9197b1ec023cdbe85
whdgur5717 <whdgur5717@gmail.com>
2024-10-24 22:28:56 +0900

    feat: add button recipe

--- packages/preset/src/preset/recipe/index.ts
+++ packages/preset/src/preset/recipe/index.ts
@@ -1,1 +1,10 @@
-export * from "./button";
+import type { RecipeConfig, RecipeVariantRecord } from "@pandacss/dev";
+
+import { button } from "./button";
+
+export const recipes: Record<
+  string,
+  Partial<RecipeConfig<RecipeVariantRecord>>
+> = {
+  button,
+};
