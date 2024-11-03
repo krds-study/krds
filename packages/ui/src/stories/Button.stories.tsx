24308e732366e49c7c5d4433832f5f3139c0a663
whdgur5717 <whdgur5717@gmail.com>
2024-11-03 18:56:18 +0900

    feat: add button component and style

--- packages/ui/src/stories/Button.stories.tsx
+++ packages/ui/src/stories/Button.stories.tsx
@@ -12,25 +12,7 @@ type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
   args: {
     children: "버튼",
-  },
-};
-
-export const Secondary = {
-  args: {
-    label: "Button",
-  },
-};
-
-export const Large = {
-  args: {
-    size: "large",
-    label: "Button",
-  },
-};
-
-export const Small = {
-  args: {
-    size: "small",
-    label: "Button",
+    disabled: true,
+    size: {},
   },
 };
